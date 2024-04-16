291969503cd720bb78972103a4e5f376e2658652
nhat0120230 <nhatnguyen22102003@gmail.com>
2024-04-16 10:32:53 +0700

    init project

---
+++ src/main/java/org/example/rentapplicationbe/RentApplicationBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.rentapplicationbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentApplicationBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentApplicationBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/rentapplicationbe/RentApplicationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rentapplicationbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentApplicationBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
