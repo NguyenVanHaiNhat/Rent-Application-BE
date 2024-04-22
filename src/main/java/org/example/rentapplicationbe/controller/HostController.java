78cd342a006b2fa667125f4867e923b32024dd54
dangphapqt1997 <dangphapqt1997@gmail.com>
2024-04-22 21:35:03 +0700

    #8-list-of-host

--- src/main/java/org/example/rentapplicationbe/controller/HostController.java
+++ src/main/java/org/example/rentapplicationbe/controller/HostController.java
@@ -3,13 +3,12 @@ package org.example.rentapplicationbe.controller;
 import org.example.rentapplicationbe.model.DTO.HostDtoDetail;
 import org.example.rentapplicationbe.services.IHostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
@@ -17,9 +16,23 @@ import java.util.List;
 public class HostController {
     @Autowired
     private IHostService iHostService;
+
     @GetMapping("/dto")
-    public ResponseEntity<List<HostDtoDetail>>findAllHost(){
-        List<HostDtoDetail>hostDtoDetails= iHostService.getHostInfor();
-        return new ResponseEntity<>(hostDtoDetails,)
+    public ResponseEntity<List<HostDtoDetail>> findAllHost() {
+        List<HostDtoDetail> hostDtoDetails = iHostService.getHostInfor();
+        return new ResponseEntity<>(hostDtoDetails, HttpStatus.OK);
+    }
+    @GetMapping("/dto/{id}")
+    public ResponseEntity<HostDtoDetail> getAllDetailHost(@PathVariable Long id) {
+        Optional<HostDtoDetail> hostDtoDetails = iHostService.findByIdHost(id);
+        if (!hostDtoDetails.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(hostDtoDetails.get(),HttpStatus.OK);
+    }
+    @PutMapping("/{id}/status/{newStatus}")
+    public ResponseEntity<String>updateStatus(@PathVariable Long id, @PathVariable String newStatus){
+        iHostService.updateAccountStatus(id,newStatus);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/org/example/rentapplicationbe/services/house/HouseService.java
+++ src/main/java/org/example/rentapplicationbe/services/house/HouseService.java
@@ -1,6 +1,7 @@
 package org.example.rentapplicationbe.services.house;
 
 import org.example.rentapplicationbe.model.Entity.House;
+import org.example.rentapplicationbe.repository.IHouseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +10,10 @@ import java.util.List;
 @Service
 public class HouseService implements IHouseService {
     @Autowired
-    private IHouseService iHouseService;
+    private IHouseRepository iHouseRepository;
 
     @Override
     public List<House> findByIdDetailHouse(Long id) {
-        return iHouseService.findByIdDetailHouse(id);
+        return iHouseRepository.findByIdDetailHouse(id);
     }
 }
