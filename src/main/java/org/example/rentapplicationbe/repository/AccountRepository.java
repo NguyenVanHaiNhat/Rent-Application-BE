d8883b6605143e48d2dbbbdbefb3a6cafc3d5eb0
toai2001 <phanmam001@gmail.com>
2024-04-25 16:21:56 +0700

    sua db

--- src/main/java/org/example/rentapplicationbe/repository/AccountRepository.java
+++ src/main/java/org/example/rentapplicationbe/repository/AccountRepository.java
@@ -5,9 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 
-
-import java.util.List;
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
