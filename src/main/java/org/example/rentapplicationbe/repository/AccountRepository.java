2f453f9f453273f41938beeb2189246e7229e534
toai2001 <phanmam001@gmail.com>
2024-04-25 16:20:12 +0700

    sua db

--- src/main/java/org/example/rentapplicationbe/repository/AccountRepository.java
+++ src/main/java/org/example/rentapplicationbe/repository/AccountRepository.java
@@ -2,8 +2,9 @@ package org.example.rentapplicationbe.repository;
 
 import org.example.rentapplicationbe.model.Entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+
 
 import java.util.List;
 import java.util.Optional;
